4f85f2217ad630050f13a2657da920d57f8ef71c
Aaron Paredes Cabrera <iron0406@outlook.com>
2023-01-09 01:53:14 -0500

    chore: lint

--- src/pages/Company/Home/Home.tsx
+++ src/pages/Company/Home/Home.tsx
@@ -1,6 +1,3 @@
-import { useQuery } from "react-query";
-import { useLoloContext } from "../../../shared/contexts/LoloProvider";
-import { getAllUsersByID } from "../../../shared/services/customer-user.service";
 import Container from "../../../ui/Container";
 
 const Home = () => {
