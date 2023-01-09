d0c782116203c5e0ce6f94d19a5cef929c6b99bc
Victor <victorhv2729@gmail.com>
2023-01-09 01:54:33 -0500

    feat: :sparkles: Date Picker

--- src/pages/Company/CompanyCobranza/CobranzaComments/CobranzaComments.tsx
+++ src/pages/Company/CompanyCobranza/CobranzaComments/CobranzaComments.tsx
@@ -1,71 +1,26 @@
 import { useState } from "react";
 import Button from "../../../../ui/Button";
 import Container from "../../../../ui/Container";
+import DatePicker from "../../../../ui/DatePicker/DatePicker";
 import TextAreaField from "../../../../ui/fields/TextAreaField";
-import Select from "../../../../ui/Select";
-import { SelectItem } from "../../../../ui/Select/interfaces";
+import moment from "moment";
 
 const CobranzaComments = () => {
-  const optionsDays: SelectItem<string, Record<string, unknown>>[] = Array.from(
-    Array(31).keys()
-  ).map((xd) => {
-    const key = `${xd + 1}`;
-    return { key, label: key };
-  });
-
-  const optionsMonths: SelectItem<string, Record<string, unknown>>[] =
-    Array.from(Array(12).keys()).map((xd) => {
-      const key = `${xd + 1}`;
-      return { key, label: key };
-    });
-
-  const currentYear = new Date().getFullYear();
-
-  const optionsYears: SelectItem<string, Record<string, unknown>>[] = [
-    { key: `${currentYear}`, label: `${currentYear}` },
-  ];
-
-  const [day, setDay] = useState("");
-  const [month, setMonth] = useState("");
-  const [year, setYear] = useState("");
-
-  const onChangeDay = (key: string) => {
-    setDay(key);
-  };
-
-  const onChangeMonth = (key: string) => {
-    setMonth(key);
-  };
-
-  const onChangeYear = (key: string) => {
-    setYear(key);
-  };
+  const [valueDate, setValueDate] = useState<string>(
+    moment(new Date()).format("DD/MM/YYYY")
+  );
 
   return (
     <Container width="100%" display="flex" flexDirection="column" gap="20px">
       <Container width="100%" display="flex" gap="5px">
-        <Select
-          label="Día"
-          width="100%"
-          value={day}
-          options={optionsDays}
-          onChange={onChangeDay}
-        />
-
-        <Select
-          label="Mes"
-          width="100%"
-          value={month}
-          options={optionsMonths}
-          onChange={onChangeMonth}
-        />
-
-        <Select
-          label="Año"
-          width="100%"
-          value={year}
-          options={optionsYears}
-          onChange={onChangeYear}
+        <DatePicker
+          required
+          placeholder="Ingrese la fecha"
+          dateFormat="DD-MM-YYYY"
+          value={valueDate}
+          getDate={(e) => {
+            setValueDate(e);
+          }}
         />
       </Container>
 
@@ -97,9 +52,7 @@ const CobranzaComments = () => {
         <TextAreaField width="100%" rows={4} />
       </Container>
 
-      <Container>
-        
-      </Container>
+      <Container></Container>
     </Container>
   );
 };
