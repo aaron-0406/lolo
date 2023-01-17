965ae50a8959ea7ecb2b0667440b251050f43873
Aaron Paredes Cabrera <iron0406@outlook.com>
2023-01-17 02:23:12 -0500

    chore: update constans

--- src/shared/utils/constant/api.ts
+++ src/shared/utils/constant/api.ts
@@ -1,4 +1,4 @@
-//export const API = "http://lolobank.com/api/v1";
-export const API = "http://localhost:5000/api/v1";
+export const API = "http://lolobank.com/api/v1";
+//export const API = "http://localhost:5000/api/v1";
 
 export const DOMAIN = "http://lolobank.com";
