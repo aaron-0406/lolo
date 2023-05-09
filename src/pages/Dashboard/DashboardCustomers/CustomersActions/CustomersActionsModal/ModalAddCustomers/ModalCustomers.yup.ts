a1640d13a0e6c07d28a7af9d193ecdc2a3f3306c
Bren pc <brayanp.cabrera@gmail.com>
2023-05-09 00:03:23 -0500

    Se modifico el numero maximo de caracteres necesarios para el ruc

--- src/pages/Dashboard/DashboardCustomers/CustomersActions/CustomersActionsModal/ModalAddCustomers/ModalCustomers.yup.ts
+++ src/pages/Dashboard/DashboardCustomers/CustomersActions/CustomersActionsModal/ModalAddCustomers/ModalCustomers.yup.ts
@@ -3,7 +3,7 @@ import yup from '../../../../../../shared/yupLocale'
 import { CustomersFirmFormType } from './hookforms.interfaces'
 
 const ModalCustomers: yup.SchemaOf<Omit<CustomersFirmFormType, 'customersfirm'>> = yup.object().shape({
-  ruc: yup.string().required().min(11).matches(/^(10|20|15|16|17)/),
+  ruc: yup.string().required().min(11).max(11).matches(/^(10|20|15|16|17)/),
   companyName: yup.string().required().min(5),
   urlIdentifier: yup.string().required().min(5),
   description: yup.string().optional(),
