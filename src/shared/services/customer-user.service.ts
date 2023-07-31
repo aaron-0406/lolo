39ec037446ee726b82254391421be971f01dae45
Bren pc <brayanp.cabrera@gmail.com>
2023-07-31 10:21:25 -0500

    espaciado entre servicios

--- src/shared/services/customer-user.service.ts
+++ src/shared/services/customer-user.service.ts
@@ -8,24 +8,30 @@ const url = `${API}/customer-user`
 export const getAll = async () => {
   return await axiosClient.get(`${url}/`)
 }
+
 export const getAllUsersByCustomerID = async (customerID: number) => {
   return await axiosClient.get(`${url}/users/${customerID}`)
 }
+
 export const getUserByUserId = async (userID: number) => {
   return await axiosClient.get(`${url}/${userID}`)
 }
+
 export const createClient = async (user: Omit<CustomerUserType, 'id'>) => {
   return await axiosClient.post(`${url}/`, user)
 }
+
 export const editUserState = async (userID: number, state: boolean) => {
   return await axiosClient.patch(`${url}/state/${userID}`, state)
 }
+
 export const editUserById = async (
   userID: number,
   user: Omit<CustomerUserType, 'id' | 'email' | 'password' | 'customerId' | 'createdAt'>
 ) => {
   return await axiosClient.patch(`${url}/${userID}`, user)
 }
+
 export const deleteUser = async (userID: number) => {
   return await axiosClient.delete(`${url}/${userID}`)
 }
