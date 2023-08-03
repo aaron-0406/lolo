517b094be9bf2011cb6a6802235f9615a43e8a08
Jefferson Paredes <jparedesm0512@gmail.com>
2023-08-02 22:17:38 -0500

    avance del eliminado del usuario

---
+++ src/pages/Dashboard/DashboardUsers/Modals/DeleteUsersModal/DeleteUsersModal.tsx
@@ -0,0 +1,82 @@
+import { useMutation } from 'react-query'
+import Container from '../../../../../ui/Container'
+import { deleteUser } from '../../../../../shared/services/customer-user.service'
+import Modal from '../../../../../ui/Modal'
+import notification from '../../../../../ui/notification'
+import Button from '../../../../../ui/Button'
+
+type DeleteUsersModalProps = {
+  visible: boolean
+  onClose: () => void
+  isEdit?: boolean
+  setLoadingGlobal: (state: boolean) => void
+  idUser?: number
+}
+
+const DeleteUsersModal = ({ visible, idUser = 0, onClose, setLoadingGlobal }: DeleteUsersModalProps) => {
+  const { isLoading: loadingDeleteUser, mutate: DeleteUserMutate } = useMutation<any, Error>(
+    async () => {
+      return await deleteUser(idUser)
+    },
+    {
+      onSuccess: () => {
+        notification({ type: 'success', message: 'Usuario eliminado' })
+        setLoadingGlobal(loadingDeleteUser)
+        onClose()
+      },
+      onError: (error: any) => {
+        notification({
+          type: 'error',
+          message: error.response.data.message,
+        })
+      },
+    }
+  )
+
+  const handleClickCloseModal = () => {
+    onClose()
+    setLoadingGlobal(false)
+  }
+
+  const DeleteUsers = () => {
+    if (idUser !== 0) {
+      DeleteUserMutate()
+    }
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      onClose={handleClickCloseModal}
+      id="modal-delete"
+      title="ELIMINAR USUARIO"
+      contentOverflowY="auto"
+      size="small"
+      footer={
+        <Container width="100%" justifyContent="space-around" display="flex" alignItems="center">
+          {
+            <Button
+              onClick={() => {
+                DeleteUsers()
+              }}
+              display="danger"
+              size="default"
+              label="ACEPTAR"
+            />
+          }
+          {
+            <Button
+              onClick={() => {
+                onClose()
+              }}
+              size="default"
+              label="CANCELAR"
+            />
+          }
+        </Container>
+      }
+    ></Modal>
+  )
+}
+
+export default DeleteUsersModal
