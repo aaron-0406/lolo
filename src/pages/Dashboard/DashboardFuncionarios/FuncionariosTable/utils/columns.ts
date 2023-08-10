7ae26a8e6089098c57184690cf701b8135e52695
Jefferson Paredes <jparedesm0512@gmail.com>
2023-08-10 11:36:03 -0500

    avance 002

---
+++ src/pages/Dashboard/DashboardFuncionarios/FuncionariosTable/utils/columns.ts
@@ -0,0 +1,28 @@
+import { ColumProps } from '../../../../../ui/Table/Table'
+
+export const funcionariosColumns: ColumProps[] = [
+  {
+    id: 'funcionarios.datatable.header.id',
+    title: 'ID',
+    width: '10%',
+    justifyContent: 'center',
+  },
+  {
+    id: 'funcionarios.datatable.header.name',
+    title: 'NOMBRE',
+    width: '40%',
+    justifyContent: 'center',
+  },
+  {
+    id: 'funcionarios.datatable.header.date',
+    title: 'FECHA DE REGISTRO',
+    width: '20%',
+    justifyContent: 'center',
+  },
+  {
+    id: 'users.datatable.header.actions',
+    title: 'ACCIONES',
+    width: '30%',
+    justifyContent: 'center',
+  },
+]
