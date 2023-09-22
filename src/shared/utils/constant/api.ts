c62eac0eea55a1351f7a980acb0afcf1c4e8db12
Aaron Paredes Cabrera <iron0406@outlook.com>
2023-09-21 23:21:17 -0500

    chore: add https

--- src/shared/utils/constant/api.ts
+++ src/shared/utils/constant/api.ts
@@ -1,5 +1,5 @@
-// export const API = 'http://lolobank.com/api/v1'
-// export const DOMAIN = 'http://lolobank.com'
+// export const API = 'https://lolobank.com/api/v1'
+// export const DOMAIN = 'https://lolobank.com'
 
 export const API = 'http://localhost:5000/api/v1'
 export const DOMAIN = 'http://localhost:5000'
