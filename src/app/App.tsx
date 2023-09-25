eb062ccfc5e24b34851311a1465068cb7e46cc09
Bren pc <brayanp.cabrera@gmail.com>
2023-09-25 15:38:08 -0500

    cuando el usuario se encuentra inactivo por veinta minutos, este se desloguea

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,22 @@
+import { useIdleTimer } from 'react-idle-timer'
 import AppRouter from '../shared/routes/AppRouter'
+import { useLoloContext } from '@/contexts/LoloProvider'
 
 const App = () => {
+  const {
+    auth: { setAuthenticate },
+  } = useLoloContext()
+
+  const onIdle = () => {
+    setAuthenticate(false)
+  }
+
+  useIdleTimer({
+    onIdle,
+    timeout: 1200_000,
+    throttle: 500,
+  })
+
   return <AppRouter />
 }
 
