c8f5a565ed8118c27736c990a182cf6a58fe7ac5
Bren pc <brayanp.cabrera@gmail.com>
2023-10-10 23:55:10 -0500

    cambios a la tabla, solo falta agregar los servicios

--- src/pages/Company/Extrajudicial/ExtrajudicialUserLogs/UserLogsTable/UserLogsTable.tsx
+++ src/pages/Company/Extrajudicial/ExtrajudicialUserLogs/UserLogsTable/UserLogsTable.tsx
@@ -1,24 +1,32 @@
+import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
+import moment from 'moment'
 import { useLoloContext } from '@/contexts/LoloProvider'
 import { getAllUserLogsByCustomerId } from '@/services/dash/user-log.service'
 import { UserLogType } from '@/types/dash/user-log.type'
+import { CustomerUserType } from '@/types/dash/customer-user.type'
 import Container from '@/ui/Container'
 import Table from '@/ui/Table'
 import BodyCell from '@/ui/Table/BodyCell'
 import EmptyStateCell from '@/ui/Table/EmptyStateCell'
-import { useQuery } from 'react-query'
 import { userLogsColumns } from './utils/columns'
-import moment from 'moment'
-import { CustomerUserType } from '@/types/dash/customer-user.type'
+import { FilterOptionsProps } from '@/ui/Table/Table'
 
 const UserLogsTable = () => {
   const {
+    bank: { selectedBank },
     client: {
       customer: { id: customerId },
     },
     customerUser: {
       user: { permissions },
     },
+    user: { users },
   } = useLoloContext()
+
+  const [filterOptions, setFilterOptions] = useState<Array<FilterOptionsProps>>([])
+  const [resetFilters, setResetFilters] = useState<boolean>(false)
+  const [selectedFilterOptions, setSelectedFilterOptions] = useState<Array<FilterOptionsProps>>([])
 
   const getPermission = (code: string) => {
     return permissions?.find((permission) => permission.code === code)
@@ -30,11 +38,108 @@ const UserLogsTable = () => {
 
   let userLogs = data?.data ?? []
 
+  const onChangeFilterOptions = (filterOption: FilterOptionsProps) => {
+    setTimeout(() => {
+      const position = selectedFilterOptions.find(
+        (selectedFilterOption) => selectedFilterOption.identifier === filterOption.identifier
+      )
+
+      if (!position) {
+        setSelectedFilterOptions((prev) => {
+          return [...prev, filterOption]
+        })
+      } else {
+        setSelectedFilterOptions((prev) => {
+          return prev.map((selectedFilterOption) => {
+            if (selectedFilterOption.identifier === filterOption.identifier) {
+              return filterOption
+            }
+
+            return selectedFilterOption
+          })
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (permissions) {
+      const optionsActions = permissions?.map((permission) => {
+        return {
+          key: permission.code,
+          label: permission.name,
+        }
+      })
+
+      setFilterOptions((prev) => {
+        const filterOption = prev.find((filter) => filter.identifier === 'user.logs.datatable.header.action')
+
+        if (filterOption) {
+          return prev.map((filter) => {
+            if (filter.identifier === 'user.logs.datatable.header.action') {
+              return {
+                identifier: filter.identifier,
+                options: optionsActions,
+              }
+            }
+            return filter
+          })
+        } else {
+          return [
+            ...prev,
+            {
+              identifier: 'user.logs.datatable.header.action',
+              options: optionsActions,
+            },
+          ]
+        }
+      })
+    }
+  }, [permissions])
+
+  useEffect(() => {
+    setSelectedFilterOptions([])
+    setResetFilters(!resetFilters)
+
+    const optionsUsers = users.map((user) => {
+      return {
+        key: user.id,
+        label: user.name,
+      }
+    })
+    setFilterOptions((prev) => {
+      const filterOption = prev.find((filter) => filter.identifier === 'user.logs.datatable.header.user')
+
+      if (filterOption) {
+        return prev.map((filter) => {
+          if (filter.identifier === 'user.logs.datatable.header.user') {
+            return {
+              identifier: filter.identifier,
+              options: optionsUsers,
+            }
+          }
+          return filter
+        })
+      } else {
+        return [
+          ...prev,
+          {
+            identifier: 'user.logs.datatable.header.user',
+            options: optionsUsers,
+          },
+        ]
+      }
+    })
+  }, [selectedBank.idCHB])
+
   return (
     <Container width="100%" height="calc(100% - 112px)" padding="20px">
       <Table
         top="260px"
         columns={userLogsColumns}
+        filterOptions={filterOptions}
+        onChangeFilterOptions={onChangeFilterOptions}
+        resetFilters={resetFilters}
         loading={isLoading}
         isArrayEmpty={!userLogs.length}
         emptyState={
