360d4f662bd04f7d6e88c2628a2f7bdd3cc880fc
aaron-0406 <iron0406@outlook.com>
2023-11-18 09:05:54 -0500

    chore: updating styles

--- src/pages/Company/Extrajudicial/ExtrajudicialCustomers/ExtrajudicialCustomers.tsx
+++ src/pages/Company/Extrajudicial/ExtrajudicialCustomers/ExtrajudicialCustomers.tsx
@@ -1,31 +1,15 @@
 import { useState } from 'react'
 import Container from '@/ui/Container'
-import Breadcrumbs from '@/ui/Breadcrumbs'
 import { Opts } from '@/ui/Pagination/interfaces'
-import { useLoloContext } from '@/contexts/LoloProvider'
 import CustomersActions from './CustomersActions'
 import CustomersTable from './CustomersTable'
-import paths from 'shared/routes/paths'
-import { LinkType } from '@/ui/Breadcrumbs/Breadcrumbs.type'
 
 const ExtrajudicialCustomers = () => {
   const [opts, setOpts] = useState<Opts>({ filter: '', limit: 50, page: 1 })
-  
-  const {
-    client: { customer },
-  } = useLoloContext()
-
-  const routers: LinkType[] = [
-    {
-      link: paths.cobranza.clientes(customer.urlIdentifier),
-      name: 'Clientes',
-    },
-  ]
 
   return (
     <Container width="100%" height="100%" display="flex" flexDirection="column">
       <CustomersActions opts={opts} setOpts={setOpts} />
-      <Breadcrumbs routes={routers} />
       <CustomersTable opts={opts} setOpts={setOpts} />
     </Container>
   )
