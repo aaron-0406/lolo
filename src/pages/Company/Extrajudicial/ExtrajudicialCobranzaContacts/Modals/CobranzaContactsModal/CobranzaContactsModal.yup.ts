3c3e17f152a9595884f96892f306893f09e10562
aaron-0406 <iron0406@outlook.com>
2023-12-05 01:00:37 -0500

    chore: changing logic to add contacts

--- src/pages/Company/Extrajudicial/ExtrajudicialCobranzaContacts/Modals/CobranzaContactsModal/CobranzaContactsModal.yup.ts
+++ src/pages/Company/Extrajudicial/ExtrajudicialCobranzaContacts/Modals/CobranzaContactsModal/CobranzaContactsModal.yup.ts
@@ -6,13 +6,27 @@ const peruPhoneNumberRegex = /^(9\d{8}|[1-7]\d{6})$/
 
 const ModalCobranzaContacts: yup.SchemaOf<Omit<ExtContactType, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>> = yup
   .object()
-  .shape({
-    name: yup.string().max(200).required(),
-    phone: yup.string().max(9).matches(peruPhoneNumberRegex, 'El número de teléfono no es válido').required(),
-    email: yup.string().max(200).email('La dirección de correo electrónico no es válida').required(),
-    state: yup.number().required(),
-    clientId: yup.number().required(),
-    customerHasBankId: yup.number().required(),
+  .shape(
+    {
+      name: yup.string().max(200).required(),
+      phone: yup
+        .string()
+        .max(9)
+        .when('phone', {
+          is: (value: string) => !!value,
+          then: yup.string().matches(peruPhoneNumberRegex, 'El número de teléfono no es válido'),
+          otherwise: yup.string().optional(),
+        }),
+      email: yup.string().max(200).email('La dirección de correo electrónico no es válida'),
+      state: yup.number().required(),
+      clientId: yup.number().required(),
+      customerHasBankId: yup.number().required(),
+    },
+    [['phone', 'phone']]
+  )
+  .test({
+    test: (value) => !!value.phone || !!value.email,
+    message: 'Debes completar al menos un campo (teléfono o correo electrónico)',
   })
 
 export const ModalCobranzaContactsResolver = yupResolver(ModalCobranzaContacts)
