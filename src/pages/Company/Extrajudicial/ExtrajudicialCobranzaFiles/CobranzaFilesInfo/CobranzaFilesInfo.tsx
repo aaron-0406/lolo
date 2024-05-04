19c1c88ad2c61d5e3d35e33697dd2dc6118c28ee
LuisSilvaB <LuisSilvaBalladares@hotmail.com>
2024-05-04 17:21:00 -0500

    chore: add new styles to info sections

--- src/pages/Company/Extrajudicial/ExtrajudicialCobranzaFiles/CobranzaFilesInfo/CobranzaFilesInfo.tsx
+++ src/pages/Company/Extrajudicial/ExtrajudicialCobranzaFiles/CobranzaFilesInfo/CobranzaFilesInfo.tsx
@@ -55,9 +55,9 @@ const CobranzaFilesInfo = ({ name, clientId }: CobranzaFilesInfoProps) => {
     >
       <Container display="flex" flexDirection="column" gap="15px">
         <Breadcrumbs routes={routers} />
-        <Container padding="0 20px 0 20px" backgroundColor="#e5e7eb">
+        <Container padding="10px" width="100%" margin="0px" backgroundColor="#eff0f6ff">
           <Text.Body size="m" weight="bold">
-            {name}
+            {name ?? '-'}
           </Text.Body>
         </Container>
       </Container>
